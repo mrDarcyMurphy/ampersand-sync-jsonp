e965a43851ba82a5147b974c0937c99a4a372fba
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-08-27 22:05:37 -0500

    ampersand-sync-jsonp mvp

---
+++ ampersand-sync-jsonp.js
@@ -0,0 +1,58 @@
+var _ = require('underscore');
+var qs = require('qs');
+
+
+// Throw an error when a URL is needed, and none is supplied.
+var urlError = function () {
+    throw new Error('A "url" property or function must be specified');
+};
+
+// arguments match ampersand-sync's so this can be used as a drop-in replacement
+module.exports = function (method, model, options) {
+
+    var config = app && app.jsonpConfig || {};
+
+    var id = _.uniqueId('jsonp_');
+
+    var script = document.createElement('script');
+
+    var params = qs.stringify(_.extend({
+        callback: '__jsonp.' + id
+    }, config.params));
+
+    function cleanup() {
+        if (script.parentNode) {
+            script.parentNode.removeChild(script);
+        }
+    }
+
+    if (!options) {
+        options = {};
+    }
+
+    if (!options.url) {
+        options.url = _.result(model, 'url') || urlError();
+    }
+
+    if (!config.baseUrl) {
+        config.baseUrl = '';
+    }
+
+    if (!window.__jsonp) {
+        window.__jsonp = {};
+    }
+
+    script.src = config.baseUrl + options.url + '?' + params;
+
+    window.__jsonp[id] = (function(success){
+        return function(resp) {
+            cleanup();
+            if (success) {
+                return success(resp);
+            }
+        }
+    })(options.success);
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+};
